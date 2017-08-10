827bb45e470724aa25cf4e92176136661d2cc015
arvinsahni <arvinsahni@arvins-MacBook-Air.local>
2017-08-10 22:03:16 +0800

    10

--- flask/run.py
+++ flask/run.py
@@ -3,4 +3,6 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+#    app.run(host='0.0.0.0', debug=True)
